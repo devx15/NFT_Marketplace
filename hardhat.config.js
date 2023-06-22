62f5822e6efa256c10e4b28594b53e1c7abf8aa4
Devansh Gupta <104734140+devx15@users.noreply.github.com>
2023-06-22 18:57:45 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: "0.8.4",
+  paths: {
+    artifacts: "./src/backend/artifacts",
+    sources: "./src/backend/contracts",
+    cache: "./src/backend/cache",
+    tests: "./src/backend/test"
+  },
+};
+// deployed address: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
